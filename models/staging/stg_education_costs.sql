92071abff29fbf0a0ed475847e5c995ea4e656a5
Jacek Walerczyk <jacek.walerczyk@contractors.roche.com>
2025-05-11 11:21:29 +0200

    adding education

---
+++ models/staging/stg_education_costs.sql
@@ -0,0 +1,22 @@
+with source as (
+    select * from {{ source('staging', 'international_education_costs') }}
+),
+
+iec as (
+    select
+        country,
+        city,
+        university,
+        program,
+        level,
+        duration_years::numeric as duration_years,
+        tuition_usd::numeric as tuition_usd,
+        living_cost_index::numeric as living_cost_index,
+        rent_usd::numeric as rent_usd,
+        visa_fee_usd::numeric as visa_fee_usd,
+        insurance_usd::numeric as insurance_usd,
+        exchange_rate::numeric as exchange_rate
+    from source
+)
+
+select * from iec
