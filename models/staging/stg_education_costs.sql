caf43950d17dcd6f673ebfdc3b4f1642edfbdcec
Jacek Walerczyk <jacek.walerczyk@contractors.roche.com>
2025-05-11 12:03:48 +0200

    fixes

--- models/staging/stg_education_costs.sql
+++ models/staging/stg_education_costs.sql
@@ -1,22 +1,15 @@
-with source as (
-    select * from {{ source('staging', 'international_education_costs') }}
-),
+select
+    country,
+    city,
+    university,
+    program,
+    level,
+    duration_years::numeric as duration_years,
+    tuition_usd::numeric as tuition_usd,
+    living_cost_index::numeric as living_cost_index,
+    rent_usd::numeric as rent_usd,
+    visa_fee_usd::numeric as visa_fee_usd,
+    insurance_usd::numeric as insurance_usd,
+    exchange_rate::numeric as exchange_rate
+from read_csv('s3://dataeng-duckdb-dbt/International_Education_Costs.csv', AUTO_DETECT=TRUE)
 
-iec as (
-    select
-        country,
-        city,
-        university,
-        program,
-        level,
-        duration_years::numeric as duration_years,
-        tuition_usd::numeric as tuition_usd,
-        living_cost_index::numeric as living_cost_index,
-        rent_usd::numeric as rent_usd,
-        visa_fee_usd::numeric as visa_fee_usd,
-        insurance_usd::numeric as insurance_usd,
-        exchange_rate::numeric as exchange_rate
-    from source
-)
-
-select * from iec
